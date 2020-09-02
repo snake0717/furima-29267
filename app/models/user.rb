a6888490c18192f0ed28c0d95ae1f3ed1eb7de18
snake0717 <69613407+snake0717@users.noreply.github.com>
2020-09-02 17:17:07 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+
+  validates :email, presence: true, uniqueness: true
+
+  validates :encrypted_password, presence: true,
+                                 length: { minimum: 6 },
+                                 format: { with: /\A[a-z0-9]+\z/ }
+
+  validates :family_name_kanji, presence: true,
+                                format: { with: /\A[一-龥]+\z/ }
+
+  validates :first_name_kanji, presence: true,
+                               format: { with: /\A[一-龥]+\z/ }
+
+  validates :family_name_katakana, presence: true,
+                                   format: { with: /\A[ァ-ヶー－]+\z/ }
+
+  validates :first_name_katakana, presence: true,
+                                  format: { with: /\A[ァ-ヶー－]+\z/ }
+
+  validates :birthday, presence: true
 end
