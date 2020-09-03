cde72b8c53981ce0054ab402337a9184e1e65eb2
snake0717 <69613407+snake0717@users.noreply.github.com>
2020-09-03 12:00:34 +0900

    ユーザー登録修正

--- db/migrate/20200901031605_devise_create_users.rb
+++ db/migrate/20200901031605_devise_create_users.rb
@@ -11,7 +11,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :first_name_kanji,     null: false
       t.string :family_name_katakana, null: false
       t.string :first_name_katakana,  null: false
-      t.string :birthday,             null: false
+      t.date   :birthday,             null: false
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_09_01_031605) do
     t.string "first_name_kanji", null: false
     t.string "family_name_katakana", null: false
     t.string "first_name_katakana", null: false
-    t.string "birthday", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
