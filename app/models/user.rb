284fc650f66e52965c059d6180c7027a124b6b7f
snake0717 <69613407+snake0717@users.noreply.github.com>
2020-09-03 14:58:30 +0900

    ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,25 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # validates :nickname, presence: true
+  validates :nickname, presence: true
 
-  # validates :email, presence: true, uniqueness: true
+  validates :email, uniqueness: true
 
-  # validates :encrypted_password, presence: true,
-  #                                length: { minimum: 6 },
-  #                                format: { with: /\A[a-z0-9]+\z/ }
+  validates :password, length: { minimum: 6 },
+                       format: { with: /\A[a-z0-9@]+\z/ }
 
-  # validates :family_name_kanji, presence: true,
-  #                               format: { with: /\A[一-龥]+\z/ }
+  validates :family_name_kanji, presence: true,
+                                format: { with: /\A[一-龥]+\z/ }
 
-  # validates :first_name_kanji, presence: true,
-  #                              format: { with: /\A[一-龥]+\z/ }
+  validates :first_name_kanji, presence: true,
+                               format: { with: /\A[一-龥]+\z/ }
 
-  # validates :family_name_katakana, presence: true,
-  #                                  format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :family_name_katakana, presence: true,
+                                   format: { with: /\A[ァ-ヶー－]+\z/ }
 
-  # validates :first_name_katakana, presence: true,
-  #                                 format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_katakana, presence: true,
+                                  format: { with: /\A[ァ-ヶー－]+\z/ }
 
-  # validates :birthday, presence: true
+  validates :birthday, presence: true
 end
