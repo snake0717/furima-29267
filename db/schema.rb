16479e7b93d0aa5899ffdc5a16fee6d14dca5432
snake0717 <69613407+snake0717@users.noreply.github.com>
2020-09-04 18:01:02 +0900

    itemテーブルの削除と作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_053015) do
+ActiveRecord::Schema.define(version: 2020_09_04_074100) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image", null: false
