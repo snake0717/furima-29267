7c371cb7d765e076de44e69bf6091012cb526f4c
snake0717 <69613407+snake0717@users.noreply.github.com>
2020-09-04 16:09:44 +0900

    itemコントローラーの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,11 +6,14 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    Item.save(item_params)
   end
 
   private
   def item_params
-    params.require(:item).permit(:name, :image, :text)
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :status_id, :shipping_charge_id,
+                                 :shipping_region_id, :shipping_day_id, :selling_price, ).
+                                 merge(user_id: current_user.id)
+
   end
 end
