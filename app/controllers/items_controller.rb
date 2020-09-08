15dd15b70cfb2e80e85d6bdd09aad9906d1d5f58
snake0717 <69613407+snake0717@users.noreply.github.com>
2020-09-08 19:37:22 +0900

    カテゴリー表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,17 +3,18 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
-    Item.save(item_params)
+    @item = Item.new(item_params)
   end
 
   private
   def item_params
     params.require(:item).permit(:image, :name, :explanation, :category_id, :status_id, :shipping_charge_id,
-                                 :shipping_region_id, :shipping_day_id, :selling_price, ).
-                                 merge(user_id: current_user.id)
+                                 :shipping_region_id, :shipping_day_id, :selling_price,)
+                                 
 
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
-  belongs_to :user
-
   extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :user
   belongs_to_active_hash :category
   belongs_to_active_hash :status
   belongs_to_active_hash :shipping_charge
