01bd606ff3c03e6528449cfba6cf7ad0d076d381
snake0717 <69613407+snake0717@users.noreply.github.com>
2020-09-09 21:53:32 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,28 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_region
   belongs_to_active_hash :shipping_day
 
+
+  validates :name , presence: true,
+                    length: {maximum: 40}
+
+  validates :explanation , presence: true,
+                           length: {maximum: 1000}
+  
+  validates :category_id , presence: true
+
+  validates :status_id , presence: true
+
+  validates :shipping_charge_id , presence: true
+
+  validates :shipping_region , presence: true
+
+  validates :shipping_day , presence: true
+
+  validates :selling_price , presence: true,
+                             format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i },
+                             numericality: {greater_than: 299},
+                             numericality: {less_than: 10000000}
+
   
 
   # #空の投稿を保存できないようにする
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    name                { 'テスト商品' }
+    explanation         { 'テスト商品の説明' }
+    category_id         { '1' }
+    status_id           { '1' }
+    shipping_charge_id  { '1' }
+    shipping_region_id  { '1' }
+    shipping_day_id     { '1' }
+    selling_price       { '500' }
   end
 end
