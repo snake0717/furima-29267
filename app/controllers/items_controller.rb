21d9b4b5c1e5f58490719d22b3460305385e8e5a
snake0717 <69613407+snake0717@users.noreply.github.com>
2020-09-09 14:15:07 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,12 +8,17 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def item_params
     params.require(:item).permit(:image, :name, :explanation, :category_id, :status_id, :shipping_charge_id,
-                                 :shipping_region_id, :shipping_day_id, :selling_price,)
+                                 :shipping_region_id, :shipping_day_id, :selling_price, :user_id)
                                  
 
   end
