d5ed9b238a47cdca25c4e979283399a32e53d45c
snake0717 <69613407+snake0717@users.noreply.github.com>
2020-09-10 18:00:32 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,32 +9,29 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_region
   belongs_to_active_hash :shipping_day
 
-
-  validates :name , presence: true,
-                    length: {maximum: 40}
-
-  validates :explanation , presence: true,
-                           length: {maximum: 1000}
   
-  validates :category_id , presence: true,
-                           numericality: { other_than: 1 } 
+  with_options presence: true do
 
-  validates :status_id , presence: true,
-                         numericality: { other_than: 1 } 
+  validates :image 
 
-  validates :shipping_charge_id , presence: true,
-                                  numericality: { other_than: 1 } 
+  validates :name , length: {maximum: 40}
 
-  validates :shipping_region_id , presence: true,
-                               numericality: { other_than: 1 } 
+  validates :explanation , length: {maximum: 1000}
+  
+  validates :category_id , numericality: { other_than: 1 } 
 
-  validates :shipping_day_id , presence: true,
-                            numericality: { other_than: 1 } 
+  validates :status_id , numericality: { other_than: 1 } 
 
-  validates :selling_price , presence: true,
-                             format: { with: /\A[0-9]+\z/ },
+  validates :shipping_charge_id , numericality: { other_than: 1 } 
+
+  validates :shipping_region_id , numericality: { other_than: 1 } 
+
+  validates :shipping_day_id , numericality: { other_than: 1 } 
+
+  validates :selling_price , format: { with: /\A[0-9]+\z/ },
                              numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
-                            
+  
+  end
 
   
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   before do
     @item = FactoryBot.build(:item)
+    @item.image = fixture_file_upload('/gorira.jpeg')
   end
 
   describe '商品出品' do
@@ -10,13 +11,15 @@ RSpec.describe Item, type: :model do
       it '全ての項目が入力されていると登録できる' do
         expect(@item).to be_valid
       end
-      it '販売価格が数字の300〜9,999,999であれば登録できる' do
-        @item.selling_price = '1000'
-        expect(@item).to be_valid
-      end
+      
     end
 
-    context '商品出品がうまくいくとき' do
+    context '商品出品がうまくいかないとき' do
+      it '商品画像が空だと登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
       it '商品名が空だと登録できない' do
         @item.name = ''
         @item.valid?
@@ -42,25 +45,50 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
+      it 'カテゴリーが---だと登録できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
       it '商品の状態が空だと登録できない' do
         @item.status_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Status can't be blank")
+      end
+      it '商品の状態が---だと登録できない' do
+        @item.status_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
       it '配送料の負担が空だと登録できない' do
         @item.shipping_charge_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping charge can't be blank")
       end
+      it '配送料の負担が---だと登録できない' do
+        @item.shipping_charge_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charge must be other than 1")
+      end
       it '発送元の地域が空だと登録できない' do
         @item.shipping_region_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping region can't be blank")
       end
+      it '発送元の地域が---だと登録できない' do
+        @item.shipping_region_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping region must be other than 1")
+      end
       it '発送までの日数が空だと登録できない' do
         @item.shipping_day_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+      end
+      it '発送元の地域が---だと登録できない' do
+        @item.shipping_day_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day must be other than 1")
       end
       it '販売価格が300より小さいと登録できない' do
         @item.selling_price = '299'
