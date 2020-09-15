b42b1c6b7d3ab4050bb74385e15a47494abff206
snake0717 <69613407+snake0717@users.noreply.github.com>
2020-09-15 10:11:18 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,7 +28,11 @@ class ItemsController < ApplicationController
 
   def destroy
     @item.destroy
-    redirect_to root_path if @item.destroy
+    if @item.destroy
+    redirect_to root_path 
+    else
+      render :show
+    end
   end
 
   private
