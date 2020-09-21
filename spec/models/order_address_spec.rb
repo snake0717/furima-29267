c1bd0a08c8746b7ba6b90983147700827cdae686
snake0717 <69613407+snake0717@users.noreply.github.com>
2020-09-21 14:20:18 +0900

    テストコード

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -12,5 +12,50 @@ RSpec.describe OrderAddress, type: :model do
         expect(@order_address).to be_valid
       end
     end
-end
+
+    context '商品購入がうまくいかないとき' do
+      it 'トークンが生成されていないと登録できない' do
+        @order_address.token = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
+      it '郵便番号が空だと登録できない' do
+        @order_address.postal_code = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it '郵便番号が数字でないと登録できない' do
+        @order_address.postal_code = 'あああ'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code is invalid")
+      end
+      it '都道府県が---だと登録できない' do
+        @order_address.shipping_region_id = '1'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Shipping region must be other than 1")
+      end
+      it '市区町村が空だと登録できない' do
+        @order_address.city = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("City can't be blank")
+      end
+      it '番地が空だと登録できない' do
+        @order_address.address = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Address can't be blank")
+      end
+      it '電話番号が空だと登録できない' do
+        @order_address.phone_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it '電話番号が数字でないと登録できない' do
+        @order_address.phone_number = 'あああ'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+      end
+
+
+    end
+  end
 end
